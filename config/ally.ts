f204329ee27ce348daac1ad59fe9c9875338645e
FranciiscoCampos170 <franciiscocampos170@gmail.com>
2022-08-02 01:02:10 +0100

    github auth

---
+++ config/ally.ts
@@ -0,0 +1,101 @@
+/**
+ * Config source: https://git.io/JOdi5
+ *
+ * Feel free to let us know via PR, if you find something broken in this config
+ * file.
+ */
+
+import Env from '@ioc:Adonis/Core/Env'
+import { AllyConfig } from '@ioc:Adonis/Addons/Ally'
+
+/*
+|--------------------------------------------------------------------------
+| Ally Config
+|--------------------------------------------------------------------------
+|
+| The `AllyConfig` relies on the `SocialProviders` interface which is
+| defined inside `contracts/ally.ts` file.
+|
+*/
+const allyConfig: AllyConfig = {
+	/*
+	|--------------------------------------------------------------------------
+	| Github driver
+	|--------------------------------------------------------------------------
+	*/
+	github: {
+		driver: 'github',
+		clientId: Env.get('GITHUB_CLIENT_ID'),
+		clientSecret: Env.get('GITHUB_CLIENT_SECRET'),
+		callbackUrl: 'http://127.0.0.1:3333/github/callback',
+	},
+	/*
+	|--------------------------------------------------------------------------
+	| Google driver
+	|--------------------------------------------------------------------------
+	*/
+	google: {
+		driver: 'google',
+		clientId: Env.get('GOOGLE_CLIENT_ID'),
+		clientSecret: Env.get('GOOGLE_CLIENT_SECRET'),
+		callbackUrl: 'http://localhost:3333/google/callback',
+	},
+	/*
+	|--------------------------------------------------------------------------
+	| Twitter driver
+	|--------------------------------------------------------------------------
+	*/
+	twitter: {
+		driver: 'twitter',
+		clientId: Env.get('TWITTER_CLIENT_ID'),
+		clientSecret: Env.get('TWITTER_CLIENT_SECRET'),
+		callbackUrl: 'http://localhost:3333/twitter/callback',
+	},
+	/*
+	|--------------------------------------------------------------------------
+	| Discord driver
+	|--------------------------------------------------------------------------
+	*/
+	discord: {
+		driver: 'discord',
+		clientId: Env.get('DISCORD_CLIENT_ID'),
+		clientSecret: Env.get('DISCORD_CLIENT_SECRET'),
+		callbackUrl: 'http://localhost:3333/discord/callback',
+	},
+  /*
+  |--------------------------------------------------------------------------
+  | LinkedIn driver
+  |--------------------------------------------------------------------------
+  */
+  linkedin: {
+    driver: 'linkedin',
+    clientId: Env.get('LINKEDIN_CLIENT_ID'),
+    clientSecret: Env.get('LINKEDIN_CLIENT_SECRET'),
+    callbackUrl: 'http://localhost:3333/linkedin/callback',
+  },
+  /*
+  |--------------------------------------------------------------------------
+  | Facebook driver
+  |--------------------------------------------------------------------------
+  */
+  facebook: {
+    driver: 'facebook',
+    clientId: Env.get('FACEBOOK_CLIENT_ID'),
+    clientSecret: Env.get('FACEBOOK_CLIENT_SECRET'),
+    callbackUrl: 'http://localhost:3333/facebook/callback',
+  },
+  /*
+  |--------------------------------------------------------------------------
+  | Spotify driver
+  |--------------------------------------------------------------------------
+  */
+  spotify: {
+    driver: 'spotify',
+    clientId: Env.get('SPOTIFY_CLIENT_ID'),
+    clientSecret: Env.get('SPOTIFY_CLIENT_SECRET'),
+    callbackUrl: 'http://localhost:3333/spotify/callback',
+  },
+
+}
+
+export default allyConfig
--- start/routes.ts
+++ start/routes.ts
@@ -18,6 +18,7 @@
 |
 */
 
+
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
@@ -27,3 +28,39 @@ Route.get('/', async ({ view }) => {
 Route.get('/auth',async ({view}) => { 
   return view.render('auth')
  })
+
+ Route.get('/github/redirect', async ({ ally }) => {
+  return ally.use('github').redirect()
+}).as('loginGithub')
+
+Route.get('github/callback', async ({ ally }) => {
+  const github = ally.use('github')
+
+  /**
+   * User has explicitly denied the login request
+   */
+  if (github.accessDenied()) {
+    return 'Access was denied'
+  }
+
+  /**
+   * Unable to verify the CSRF state
+   */
+  if (github.stateMisMatch()) {
+    return 'Request expired. Retry again'
+  }
+
+  /**
+   * There was an unknown error during the redirect
+   */
+  if (github.hasError()) {
+    return github.getError()
+  }
+
+  /**
+   * Finally, access the user
+   */
+  const user = await github.user()
+}).as('callback')
+
+
